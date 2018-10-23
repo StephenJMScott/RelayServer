841d98aed6496153401667df7b765fc93f4e65ac
Stephen Scott <stephenjmscott@gmail.com>
2018-10-23 10:37:19 +0100

    Simple Local Server Created to Test Concept 23/10/2018

---
+++ Downloads/RelayServer/src/relayserver/Client.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package relayserver;
+import java.io.*;
+import java.net.*;
+
+/**
+ *
+ * @author StephenJMScott
+ */
+public class Client {
+    public static final String DefaultHost = "localhost"; //change this later;
+    public static final int DefaultPort = 2001; // may need to include Port 2002 as well. 
+    
+    
+    private static String host = DefaultHost;
+    private static int port = DefaultPort;
+    
+    
+    public static void main(String[] args) throws Exception{
+        
+        Socket S = new Socket(host, port);                                      
+        DataOutputStream  DataOut = new DataOutputStream(S.getOutputStream());
+        BufferedReader br = new BufferedReader( new InputStreamReader(System.in));
+            while (true){  //this will run indefinetly 
+                String sout = br.readLine();
+                DataOut.writeUTF(sout);
+                    if(sout.equalsIgnoreCase("exit"))
+                        break;
+                
+            }
+            S.close();
+        
+        
+        
+        
+    }
+        
+        
+}
--- Downloads/RelayServer/src/relayserver/RelayServer.java
+++ Downloads/RelayServer/src/relayserver/RelayServer.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package relayserver;
+import java.io.*;
+import java.net.*;
+
 
 /**
  *
@@ -12,10 +15,28 @@ package relayserver;
  * 22/10/2018
  */
 public class RelayServer {
+    
+    
 
     
-    public static void main(String[] args) {
-        //something to add
+    public static void main(String[] args) throws Exception{
+       
+        ServerSocket ss = new ServerSocket (Client.DefaultPort);
+        Socket s = ss.accept();
+        System.out.println("Connected");
+        DataInputStream DataInput =  new DataInputStream(s.getInputStream());
+        BufferedReader BR = new BufferedReader(new InputStreamReader(System.in));
+        
+        while(true){  //Run indefintely 
+            String foo = DataInput.readUTF();
+            System.out.println("Client Message: " + foo);
+                       if(foo.equalsIgnoreCase("exit"))
+                           break;
+            
+        }
+        s.close();
+        
+        
     }
     
 }
